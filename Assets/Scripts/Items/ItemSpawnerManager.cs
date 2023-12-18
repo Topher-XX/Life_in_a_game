0c847417085c75266a81b33a9a4de288f5217efa
Topher-XX <christopher.hussenet@sfr.fr>
2023-12-18 19:52:38 +0100

    Spawn items, monsters and chests in secret rooms + code optimization

--- Assets/Scripts/Items/ItemSpawnerManager.cs
+++ Assets/Scripts/Items/ItemSpawnerManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.Progress;
 
 public class ItemSpawnerManager : MonoBehaviour
 {
@@ -11,15 +10,26 @@ public class ItemSpawnerManager : MonoBehaviour
     [SerializeField] private GameObject[] items;
 
     [Header("Items Rarity Multiplicator")]
-    [SerializeField] private static int mPrimaryStat;//Primary stat multiplicator
-    [SerializeField] private static int mSecondaryStat;//Secondary stat multiplicator
-    [SerializeField] private static int mDirectStat;//Direct stat multiplicator
+    [SerializeField] private float mPrimaryStat;//Primary stat multiplicator
+    [SerializeField] private float mSecondaryStat;//Secondary stat multiplicator
+    [SerializeField] private float mDirectStat;//Direct stat multiplicator
 
     [Header("Bonus Item")]
     [Tooltip("Luck stat to have for ensure a Item bonus.\n" +
              "If the luck stat if inferior of the tier, there are a random chance to get a bonus item.")]
     [SerializeField] private int itemBonusTier;
 
+    public static ItemSpawnerManager instance;
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Warning ! They are more than 1 ItemSpawnerManager in this scene !");
+            return;
+        }
+        instance = this;
+    }
 
     private void Start()
     {
@@ -27,11 +37,18 @@ public class ItemSpawnerManager : MonoBehaviour
         
     }
 
-
-    public void SpawnSpecificNbItems(int _nbItemToSpawn, Vector3 _spawnPosition, Quaternion _spawnRotation)
+    /// <summary>
+    /// spawn a number of items requested at a given position
+    /// </summary>
+    /// <param name="_nbItemToSpawn"></param>
+    /// <param name="_spawnPosition"></param>
+    /// <param name="randomPos"></param>
+    /// <param name="_spawnRotation"></param>
+    public void SpawnSpecificNbItems(int _nbItemToSpawn, Vector3 _spawnPosition, float randomPos, Quaternion _spawnRotation)
     {
         List<GameObject> itemsCanBeSpawned = new List<GameObject>(items);
 
+        //Check is the number items to spawn is superior than the number items in the list
         if (_nbItemToSpawn > itemsCanBeSpawned.Count)
         {
             _nbItemToSpawn = itemsCanBeSpawned.Count;
@@ -39,46 +56,62 @@ public class ItemSpawnerManager : MonoBehaviour
                              "Nb Item to spawn is resize to nb item in the array");
         }
 
-
+        //Spawn items depending of the request
         for (int i = 0; i < _nbItemToSpawn; i++)
         {
-            int maxRandom = 0;
+            //Set the maxRandom
+            float maxRandom = 0;
             foreach (GameObject item in itemsCanBeSpawned)
             {
                 maxRandom += ItemRarity(item.GetComponent<Item>());
             }
 
-            int random = Random.Range(0, maxRandom);
+            //Set a random value depending of the maxRandom for the item's spawn
+            float random = Random.Range(0, maxRandom);
 
-            int checkRandom = 0;
+            #region Spawn a item from the random value
+            float checkRandom = 0;
             for (int j = 0; j < itemsCanBeSpawned.Count; j++)
             {
                 checkRandom += ItemRarity(itemsCanBeSpawned[j].GetComponent<Item>());
-                if (random <= checkRandom)
+                if (random <= checkRandom) //true -> it's the item chosen
                 {
-                    SpawnItem(itemsCanBeSpawned[j], _spawnPosition, _spawnRotation);
+                    SpawnItem(itemsCanBeSpawned[j], _spawnPosition, randomPos, _spawnRotation);
+                    itemsCanBeSpawned.Remove(itemsCanBeSpawned[j]);
+                    break;
+                }
+                else if (random > maxRandom) //prevent the float imprecision
+                {
+                    SpawnItem(itemsCanBeSpawned[j], _spawnPosition, randomPos, _spawnRotation);
                     itemsCanBeSpawned.Remove(itemsCanBeSpawned[j]);
                     break;
                 }
             }
-
+            #endregion
 
 
         }
     }
 
-    //Spawn items from a number
-    private void SpawnItem(GameObject itemChosen, Vector3 _spawnPosition, Quaternion _spawnRotation)
+    /// <summary>
+    /// Spawn an element at a given position
+    /// </summary>
+    /// <param name="itemChosen"></param>
+    /// <param name="_spawnPosition"></param>
+    /// <param name="randomPos"></param>
+    /// <param name="_spawnRotation"></param>
+    private void SpawnItem(GameObject itemChosen, Vector3 _spawnPosition, float randomPos, Quaternion _spawnRotation)
     {
-        float x = Random.Range(-10f, 10f);
-        float y = Random.Range(-10f, 10f);
+        float x = Random.Range(-randomPos, randomPos);
+        float y = Random.Range(-randomPos, randomPos);
         Vector3 spawnPosition = _spawnPosition + new Vector3(x, y, _spawnPosition.z);
 
         Instantiate(itemChosen, spawnPosition, _spawnRotation);
-
     }
 
-    //Determine the number of Item to spawn
+    /// <summary>
+    /// Determine the number of objects to spawn based on the player's chance stat
+    /// </summary>
     public int AddBonusItemToSpawn(int _luckState)
     {
         int nbBonusItemsToSpawn = (_luckState / itemBonusTier);
@@ -88,7 +121,12 @@ public class ItemSpawnerManager : MonoBehaviour
         return nbBonusItemsToSpawn;
     }
 
-    public int ItemRarity(Item item)
+    /// <summary>
+    /// return the rarity of the item
+    /// </summary>
+    /// <param name="item"></param>
+    /// <returns></returns>
+    public float ItemRarity(Item item)
     {
         return (item.Strength + item.Dexterity + item.Intelligence) * mPrimaryStat +
                (item.Charism + item.Luck) * mSecondaryStat +
--- Assets/Scripts/Procedural/MapGenerator.cs
+++ Assets/Scripts/Procedural/MapGenerator.cs
@@ -5,12 +5,13 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
+//Use for determine the door's orientation for secrets rooms
 public enum OrientationEnterSecretRoom
 {
     up, down, left, right
 }
 
-public class MapGenerator : MonoBehaviour
+public partial class MapGenerator : MonoBehaviour
 {
     [Header("Tile Map")]
     [SerializeField] private Tilemap floorMap;
@@ -25,189 +26,71 @@ public class MapGenerator : MonoBehaviour
     [SerializeField] private Tile wallTile;
     [SerializeField] private Tile destructibleWallTile;
 
-    [Header("Secret Room Parameters")]
-    [SerializeField] private int nbSecretRoom;
-    [SerializeField] private int secretRoomSize;
-    [SerializeField] private GameObject[] ObjectInRoom;
-
     private void Start()
     {
-        GenerateFloor();
-        GenerateWall();
+        //Create the map's arena
+        GenerateFloor(0, 0, mapLength, mapHeight);
+        GenerateWall(0, 0, mapLength, mapHeight);
+
         GenerateSecretRoom();
     }
 
-    private void GenerateFloor()
+    /// <summary>
+    /// generates floors based on position and room size
+    /// </summary>
+    /// <param name="startXCoordinate"></param>
+    /// <param name="startYCoordinate"></param>
+    /// <param name="length"></param>
+    /// <param name="height"></param>
+    private void GenerateFloor(int startXCoordinate, int startYCoordinate, int length, int height)
     {
-        //Generate floor in a rectangular form with a size of mapLength x mapHeight
-        for (int i = 0; i < mapLength; i++)
+        int endXCoordiante = startXCoordinate + length - 1;
+        int endYCoordiante = startYCoordinate + height - 1;
+
+        //Generate floor in a rectangular form with a size of length x height
+        for (int i = startXCoordinate; i <= endXCoordiante; i++)
         {
-            for (int j = 0; j < mapHeight; j++)
+            for (int j = startYCoordinate; j <= endYCoordiante; j++)
             {
-                Vector3Int position = new Vector3Int(i - mapLength/2, j - mapHeight/2, 0);
+                Vector3Int position = new Vector3Int(i, j, 0);
                 floorMap.SetTile(position, floorTile);
             }
         }
     }
 
-    private void GenerateWall()
+    /// <summary>
+    /// generates walls based on position and room size
+    /// </summary>
+    /// <param name="startXCoordinate"></param>
+    /// <param name="startYCoordinate"></param>
+    /// <param name="length"></param>
+    /// <param name="height"></param>
+    private void GenerateWall(int startXCoordinate, int startYCoordinate, int length, int height)
     {
+        int endXCoordinate = startXCoordinate + length - 1;
+        int endYCoordinate = startYCoordinate + height - 1;
+
         //Generate wall on the top and bottow of the map
-        for (int i = 0; i < mapLength; i++)
+        for (int i = startXCoordinate; i <= endXCoordinate; i++)
         {
-            Vector3Int downPosition = new Vector3Int(i - mapLength / 2, -mapHeight / 2, 0);
+            Vector3Int downPosition = new Vector3Int(i, startYCoordinate, 0);
             wallMap.SetTile(downPosition, wallTile);
 
-            Vector3Int upPosition = new Vector3Int(i - mapLength / 2, mapHeight / 2 - 1, 0);
+            Vector3Int upPosition = new Vector3Int(i, endYCoordinate, 0);
             wallMap.SetTile(upPosition, wallTile);
         }
 
         //Generate wall on left and right of the map
-        for (int i = 0; i < mapHeight; i++)
+        for (int i = startYCoordinate; i <= endYCoordinate; i++)
         {
-            Vector3Int leftPosition = new Vector3Int(-mapLength / 2, i - mapHeight / 2, 0);
+            Vector3Int leftPosition = new Vector3Int(startXCoordinate, i, 0);
             wallMap.SetTile(leftPosition, wallTile);
 
-            Vector3Int rightPosition = new Vector3Int(mapLength / 2 - 1, i - mapHeight / 2, 0);
+            Vector3Int rightPosition = new Vector3Int(endXCoordinate, i, 0);
             wallMap.SetTile(rightPosition, wallTile);
         }
     }
 
-    private void GenerateSecretRoom()
-    {
-        //determine nb secret room can ba placed
-        //the nb is dependante of the size's map and the size of a secret room
-        //bigger is the map and smaller is a secret room, more secret room are created
-        //the enter of a secret room can't be create too close of the maps' corners
-        int nbSecretRoomOnALength = mapLength / secretRoomSize;
-        int nbSecretRoomOnAHeight = mapHeight / secretRoomSize;
-
-        //Check if the Number Secret Room is not greater than the map can have.
-        int maxSecretRoomPossible = nbSecretRoom;
-        if (maxSecretRoomPossible > (nbSecretRoomOnAHeight + nbSecretRoomOnALength) * 2)
-        {
-            maxSecretRoomPossible = (nbSecretRoomOnAHeight + nbSecretRoomOnALength) * 2;
-
-            Debug.LogWarning("the number Secret Room requested if greater than " +
-                             "the map can contain !\n" +
-                             "Reduce the Number of Secret Room requested or extend the map's size.\n" +
-                             "Number Secret Room is Created with the maximum possible : " +
-                             maxSecretRoomPossible);
-
-        }
-
-        //All Available Secret Room Position possible
-        Dictionary<Vector3Int, OrientationEnterSecretRoom> availableSpotSecretRoom = new Dictionary<Vector3Int, OrientationEnterSecretRoom>();
-        
-        //Get all available position on top and bottom
-        for (int i = 0; i < nbSecretRoomOnALength; i++)
-        {
-            int xPos = i * secretRoomSize - mapLength / 2 + secretRoomSize / 2;
-            
-            Vector3Int topPos = new Vector3Int(xPos, -mapHeight / 2, 0);
-            availableSpotSecretRoom.Add(topPos, OrientationEnterSecretRoom.up);
-
-            Vector3Int bottomPos = new Vector3Int(xPos, mapHeight / 2 - 1, 0);
-            availableSpotSecretRoom.Add(bottomPos, OrientationEnterSecretRoom.down);
-        }
-
-        //Get all available position on left and right
-        for (int i = 0; i < nbSecretRoomOnAHeight; i++)
-        {
-            int yPos = i * secretRoomSize - mapHeight / 2 + secretRoomSize / 2;
-
-            Vector3Int leftPos = new Vector3Int(-mapLength/2, yPos, 0);
-            availableSpotSecretRoom.Add(leftPos, OrientationEnterSecretRoom.left);
-
-            Vector3Int rightPos = new Vector3Int(mapLength/2 - 1, yPos, 0);
-            availableSpotSecretRoom.Add(rightPos, OrientationEnterSecretRoom.right);
-        }
 
 
-        //Spawn a number of secret room on availables positions
-        for (int i = 0; i < maxSecretRoomPossible; i++)
-        {
-            int selectedPosIndex = Random.Range(0, availableSpotSecretRoom.Count);
-            
-            Vector3Int posEnterSecretRoom = availableSpotSecretRoom.ElementAt(selectedPosIndex).Key;
-            OrientationEnterSecretRoom orientationEnterSecretRoom =
-                availableSpotSecretRoom.ElementAt(selectedPosIndex).Value;
-
-
-            int minXPosRoom;
-            int maxXPosRoom;
-            int minYPosRoom;
-            int maxYPosRoom;
-
-            //determine orientation for the room
-            //Up
-            if (orientationEnterSecretRoom == OrientationEnterSecretRoom.up)
-            {
-                minXPosRoom = posEnterSecretRoom.x - (secretRoomSize / 2);
-                maxXPosRoom = posEnterSecretRoom.x + (secretRoomSize / 2);
-                minYPosRoom = posEnterSecretRoom.y - (secretRoomSize - 1);
-                maxYPosRoom = posEnterSecretRoom.y;
-            }
-            //Down
-            else if (orientationEnterSecretRoom == OrientationEnterSecretRoom.down)
-            {
-                minXPosRoom = posEnterSecretRoom.x - (secretRoomSize / 2);
-                maxXPosRoom = posEnterSecretRoom.x + (secretRoomSize / 2);
-                minYPosRoom = posEnterSecretRoom.y;
-                maxYPosRoom = posEnterSecretRoom.y + (secretRoomSize - 1);
-            }
-            //Left
-            else if (orientationEnterSecretRoom == OrientationEnterSecretRoom.left)
-            {
-                minXPosRoom = posEnterSecretRoom.x - (secretRoomSize - 1);
-                maxXPosRoom = posEnterSecretRoom.x;
-                minYPosRoom = posEnterSecretRoom.y - (secretRoomSize / 2);
-                maxYPosRoom = posEnterSecretRoom.y + (secretRoomSize / 2);
-            }
-            //Right
-            else
-            {
-                minXPosRoom = posEnterSecretRoom.x;
-                maxXPosRoom = posEnterSecretRoom.x + (secretRoomSize - 1);
-                minYPosRoom = posEnterSecretRoom.y - (secretRoomSize / 2);
-                maxYPosRoom = posEnterSecretRoom.y + (secretRoomSize / 2);
-            }
-
-            //Create floor
-            for (int j = minXPosRoom; j <= maxXPosRoom; j++)
-            {
-                for (int k = minYPosRoom; k <= maxYPosRoom; k++)
-                {
-                    Vector3Int pos = new Vector3Int(j, k, 0);
-                    floorMap.SetTile(pos, floorTile);
-                }
-            }
-
-            //Create Wall
-            for (int j = minXPosRoom; j <= maxXPosRoom; j++)
-            {
-                Vector3Int downPosition = new Vector3Int(j, minYPosRoom, 0);
-                wallMap.SetTile(downPosition, wallTile);
-
-                Vector3Int upPosition = new Vector3Int(j, maxYPosRoom, 0);
-                wallMap.SetTile(upPosition, wallTile);
-            }
-
-            for (int j = minYPosRoom; j <= maxYPosRoom; j++)
-            {
-                Vector3Int leftPosition = new Vector3Int(minXPosRoom, j, 0);
-                wallMap.SetTile(leftPosition, wallTile);
-
-                Vector3Int rightPosition = new Vector3Int(maxXPosRoom, j, 0);
-                wallMap.SetTile(rightPosition, wallTile);
-            }
-
-            wallMap.SetTile(posEnterSecretRoom, destructibleWallTile);
-
-            availableSpotSecretRoom.Remove(posEnterSecretRoom);
-
-        }
-        
-
-    }
 }
